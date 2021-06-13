afd94622e401a80d8a847cc2a0c3e23847033225
Matheus Santos <matheussm301@gmail.com>
2021-06-12 21:47:14 -0300

    remove dist

--- dist/model/Category.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Category = void 0;
-var Category = /** @class */ (function () {
-    function Category() {
-    }
-    return Category;
-}());
-exports.Category = Category;
